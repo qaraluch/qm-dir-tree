a85bcff728c6087bfa7f4dd5d0c98090d747567f
qaraluch <prezenciq@gmail.com>
2018-05-12 11:48:51 +0200

    Add dependecies

--- index.js
+++ index.js
@@ -1,7 +1,173 @@
-function XXX() {
-  return {};
+const nodePath = require("path");
+const { walkProcessed } = require("qm-walk");
+
+function* idGenerator(initialValue = 0) {
+  let id = initialValue;
+  while (true) {
+    yield id++;
+  }
+}
+
+const cwd = process.cwd();
+
+function buildEntries(walkOutput) {
+  let entries = {};
+  const idEntries = idGenerator(0);
+  const itemIndexing = item => {
+    let fileID = idEntries.next().value;
+    entries[fileID] = {
+      id: fileID,
+      name: item.name,
+      path: item.path,
+      stats: item.stats,
+      cwd: item.cwd,
+      crown: item.crown,
+      parent: item.parent,
+      isFile: item.isFile
+    };
+  };
+  walkOutput.map(itemIndexing);
+  return entries;
+}
+
+function buildTree(walkOutput, entries, pathResolved) {
+  function findItemInChildrens(toFind, childrenArr) {
+    const reducer = (acc, next) => (next.item === toFind ? true : acc);
+    return childrenArr.reduce(reducer, false);
+  }
+
+  function getParentOfItem(toFind, path) {
+    const idx = path.split("/").findIndex(el => el === toFind);
+    const result = path
+      .split("/")
+      .slice(idx - 1, idx)
+      .join();
+    return result;
+  }
+
+  function findItemIdInEntries(toFind, referencePath, entries) {
+    function examineItem(entry) {
+      const nameConfirmation = entry.name === toFind;
+      const entryParent = getParentOfItem(toFind, entry.path);
+      const referenceParent = getParentOfItem(toFind, referencePath);
+      const parentConfirmation = entryParent === referenceParent;
+      return nameConfirmation && parentConfirmation;
+    }
+    const reducer = (accId, nextId) => {
+      const entry = entries[nextId];
+      const result = examineItem(entry) ? nextId : accId;
+      return result;
+    };
+    return Object.keys(entries).reduce(reducer, -1);
+  }
+
+  const treeReducer = (tree, nextItem) => {
+    let nextPath = nextItem.crown; //for build nodes
+    let referencePath = nextItem.path; //help for findItemInEntries
+    let nextIsFile = nextItem.isFile; // for node type construction
+    let pointer = tree; // v- for traversing path
+    nextPath.split("/").forEach(item => {
+      if (item !== "") {
+        // ^- first item is empty because of first "/"
+        const itemId = findItemIdInEntries(item, referencePath, entries);
+        if (!findItemInChildrens(itemId, pointer.childrens)) {
+          nextIsFile
+            ? pointer.childrens.push({ item: itemId }) // new node for file
+            : pointer.childrens.push({ item: itemId, childrens: [] }); // new node for dir
+        }
+        let nextPointer = pointer.childrens.filter(el => el.item === itemId);
+        pointer = nextPointer[0];
+      }
+    });
+    return tree;
+  };
+
+  return walkOutput.reduce(treeReducer, {
+    root: pathResolved,
+    childrens: []
+  });
+}
+
+//Traverse Methods:
+function _traverse(options = {}) {
+  const {
+    tree: { childrens },
+    entries
+  } = this;
+  const defaultCb = (entry, item, childrens) =>
+    console.log(item, entry.name, childrens && childrens.length); // eslint-disable-line no-console
+  let { cb = defaultCb, deep = 0 } = options; // deep = 0 -> no deep limit
+  let nextChildrens;
+  let currentDeep = 1;
+  function traverseIteration(cls) {
+    const ifGoDeeper = deep <= 0 ? true : currentDeep < deep; //'deep <= 0' for no limit
+    currentDeep++;
+    cls.forEach(treeItem => {
+      nextChildrens = treeItem.childrens && treeItem.childrens;
+      cb(entries[treeItem.item], treeItem.item, treeItem.childrens); //childrens may be undefined
+      treeItem.childrens && (ifGoDeeper && traverseIteration(nextChildrens));
+    });
+  }
+  traverseIteration(childrens);
+}
+
+function _findItemChildrens(id) {
+  let result;
+  const cb = (entry, item, childrens) => {
+    item === id && (result = childrens);
+  };
+  _traverse.call(this, { deep: 0, cb });
+  return result;
+}
+
+function getChildrensOfNode({ id, filter = "all", whatToReturn = false } = {}) {
+  const filterShielded =
+    filter === "files" ? "files" : filter === "dirs" ? "dirs" : "all";
+  const nodeChildrens = _findItemChildrens.call(this, id);
+  let filteredChildrens;
+  if (filterShielded === "files") {
+    filteredChildrens =
+      nodeChildrens &&
+      nodeChildrens.filter(itm => this.entries[itm.item].isFile);
+  } else if (filterShielded === "dirs") {
+    filteredChildrens =
+      nodeChildrens &&
+      nodeChildrens.filter(itm => !this.entries[itm.item].isFile);
+  } else {
+    filteredChildrens = nodeChildrens;
+  }
+  let result;
+  if (whatToReturn) {
+    result = filteredChildrens
+      .map(itm => this.entries[itm.item])
+      .map(whatToReturn);
+  } else {
+    result = filteredChildrens;
+  }
+  return result;
+}
+
+const treeMethods = {
+  getChildrensOfNode
+};
+
+async function dirTree({ path = cwd } = {}) {
+  try {
+    let dirTreeObject = {};
+    const pathResolved = nodePath.resolve(cwd, path);
+    const walkOutput = await walkProcessed({ path: pathResolved });
+    const entries = buildEntries(walkOutput);
+    const tree = buildTree(walkOutput, entries, pathResolved);
+    dirTreeObject = { ...treeMethods, walk: walkOutput, entries, tree };
+    return dirTreeObject;
+  } catch (error) {
+    throw new Error(
+      `Sth went wrong with 'dir-tree' module: \n
+        ${error.stack} \n      `
+    );
+  }
 }
 
 module.exports = {
-  XXX
+  dirTree
 };
--- index.test.js
+++ index.test.js
@@ -1,24 +1,173 @@
 const test = require("ava");
 const path = require("path");
-const { XXX } = require("../index.js");
+const { dirTree } = require("../index.js");
 
-test.only("is function", t => {
+const dirTreeTestPath = "./lib/walk/test/fixtures/";
+
+let dirTreeOutput;
+
+test.before(async () => {
+  dirTreeOutput = await dirTree({ path: dirTreeTestPath });
+});
+
+test("dirTree is function", t => {
   const msg = "should be a function ";
-  const actual = typeof XXX === "function";
+  const actual = typeof dirTree === "function";
   const expected = true;
   t.is(actual, expected, msg);
 });
 
 test("return promise", async t => {
   const msg = "should return a promise ";
-  const actual = typeof (await XXX().then) === "function";
+  const actual = typeof (await dirTree().then) === "function";
   const expected = true;
   t.is(actual, expected, msg);
 });
 
-test("...", async t => {
-  const msg = "should ...";
-  const actual = await XXX();
-  const expected = "";
+test("get walk array", async t => {
+  const msg = "should return object with walk property which is array";
+  const actual = dirTreeOutput.walk instanceof Array;
+  const expected = true;
+  t.is(actual, expected, msg);
+  const msg2 = "should return walk property which is array with length > 0";
+  const actual2 = dirTreeOutput.walk.length > 0;
+  const expected2 = true;
+  t.is(actual2, expected2, msg2);
+});
+
+test("build entries", async t => {
+  const msg = "should return object with walk property which is Object";
+  const actual = dirTreeOutput.entries instanceof Object;
+  const expected = true;
+  t.is(actual, expected, msg);
+  const msg2 = "should return walk property which is array with length > 0";
+  const actual2 = Object.keys(dirTreeOutput.entries).length > 0;
+  const expected2 = true;
+  t.is(actual2, expected2, msg2);
+  const msg3 = "should entry has name property which is a String";
+  const actual3 = typeof dirTreeOutput.entries[3].name === "string";
+  const expected3 = true;
+  t.is(actual3, expected3, msg3);
+});
+
+test("build normalized tree", async t => {
+  const msg = "should return object with tree property which is Object";
+  const actual = dirTreeOutput.tree instanceof Object;
+  const expected = true;
+  t.is(actual, expected, msg);
+  const msg2 =
+    "should return tree property witch childrens property which is array with length > 0";
+  const actual2 = Object.keys(dirTreeOutput.tree.childrens).length > 0;
+  const expected2 = true;
+  t.is(actual2, expected2, msg2);
+});
+
+//helpers for test:
+function rebuildTreePaths(tree, entries) {
+  const getPath = id => entries[id].path;
+  const { childrens } = tree;
+  let newWalk = [];
+  function traverse(childrensArr) {
+    childrensArr.forEach(tRecord => {
+      newWalk.push(getPath(tRecord.item));
+      tRecord.childrens && traverse(tRecord.childrens);
+    });
+  }
+  traverse(childrens);
+  return newWalk;
+}
+
+function compareArrays(walkOriginal, walkRebuild) {
+  const checkIfExists = walkRebuild => (item, idx) => {
+    const compare = walkRebuild.includes(item.path);
+    return compare;
+  };
+  const result = walkOriginal
+    .map(checkIfExists(walkRebuild))
+    .reduce((acc, next) => acc && next);
+  return result;
+}
+
+test("reverse path-tree rebuild", async t => {
+  const msg = "should arrays be the same";
+  const walkOriginal = dirTreeOutput.walk;
+  const walkRebuild = rebuildTreePaths(
+    dirTreeOutput.tree,
+    dirTreeOutput.entries
+  );
+  const actual = compareArrays(walkOriginal, walkRebuild);
+  const expected = true;
+  t.is(actual, expected, msg);
+});
+
+test("root path", async t => {
+  const msg = "should be the same as passed path";
+  const actual = dirTreeOutput.tree.root;
+  const expected = nodePath.resolve(dirTreeTestPath);
+  t.is(actual, expected, msg);
+});
+
+test("error", async t => {
+  const msg = "should throw an error";
+  const error = await t.throws(dirTree({ path: "./lib/walk/test/fixtures2/" }));
+  t.is(
+    error.message.slice(0, 38) + " ...",
+    "Sth went wrong with 'dir-tree' module: ...",
+    msg
+  );
+});
+
+test("traverse - all items of node", async t => {
+  const msg = "should return array of 5 items of of item id 11";
+  const items = dirTreeOutput.getChildrensOfNode({ id: "11" });
+  const actual = items.length;
+  const expected = 5;
+  t.is(actual, expected, msg);
+});
+
+test("traverse - no items of node", async t => {
+  const msg = "should return undefined when node not found or has no childrens";
+  const actual = dirTreeOutput.getChildrensOfNode({ id: "1111" }); //TODOC: have to be in parentheses
+  const expected = undefined;
+  t.is(actual, expected, msg);
+  const actual2 = dirTreeOutput.getChildrensOfNode({ id: "12" });
+  t.is(actual2, expected, msg);
+});
+
+test("traverse - all file items of node", async t => {
+  const msg = "should return array of 3 file items of of item id 11";
+  const items = dirTreeOutput.getChildrensOfNode({ id: "11", filter: "files" });
+  const actual = items.length;
+  const expected = 3;
+  t.is(actual, expected, msg);
+});
+
+test("traverse - all dir items of node", async t => {
+  const msg = "should return array of 2 dir items of of item id 11";
+  const items = dirTreeOutput.getChildrensOfNode({ id: "11", filter: "dirs" });
+  const actual = items.length;
+  const expected = 2;
+  t.is(actual, expected, msg);
+});
+
+test("traverse - when wrong filter value returns all items of node", async t => {
+  const msg = "should return array of 5 items of of item id 11";
+  const items = dirTreeOutput.getChildrensOfNode({
+    id: "11",
+    filter: "terefere"
+  });
+  const actual = items.length;
+  const expected = 5;
+  t.is(actual, expected, msg);
+});
+
+test("traverse - all file names of node", async t => {
+  const msg = "should return name 'thread1' of the item id 11";
+  const namesMapper = entry => entry.name;
+  const actual = dirTreeOutput.getChildrensOfNode({
+    id: "11",
+    whatToReturn: namesMapper
+  })[0];
+  const expected = "thread1.md";
   t.is(actual, expected, msg);
 });
